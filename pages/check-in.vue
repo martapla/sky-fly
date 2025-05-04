0580ade652ec6acbba80ea2ea7fb3f44fb475724
martapla <martaplanella@hotmail.com>
2025-05-04 23:56:42 +0200

    styles responsive

--- pages/check-in.vue
+++ pages/check-in.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="min-h-screen bg-gray-100 flex flex-col items-center space-y-8 py-10 px-4 bg-cover bg-center"
+  <div class="min-h-screen bg-gray-100 flex flex-col items-center space-y-8 sm:py-4 md:py-10 px-4 bg-cover bg-center"
     style="background-image: url('/images/sky.jpg')">
     <NuxtLink to="/" class="absolute top-4 left-4 text-white text-lg hover:text-pink-500">
-      ← Back Home
+      Home
     </NuxtLink>
 
-    <h1 class="mb-8 text-white sky-title">Sky Fly</h1>
+    <h1 class="sm:mb-0 md:mb-8 text-white sky-title">Sky Fly</h1>
 
     <CheckInForm />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    class="min-h-screen bg-gray-100 flex flex-col items-center space-y-8 py-10 px-4 bg-cover bg-center"
+    class="min-h-screen bg-gray-100 flex flex-col items-center sm:pt-4 space-y-8 sm:py-4 md:py-10 px-4 bg-cover bg-center"
     style="background-image: url('/images/sky.jpg')">
 
-    <h1 class="mb-8 text-white sky-title">Sky Fly</h1>
+    <h1 class="sm:mb-0 md:mb-8 text-white sky-title">Sky Fly</h1>
 
     <div class="w-full max-w-xl">
       <FlightBookingForm />
